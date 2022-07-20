0e0d8bbcfdc52a962f478e16dc43b8941d458fba
OsamaBytes <osama.ahmed@46bytes.com>
2022-07-20 12:35:40 +0500

    Added Interface for Login DTO and Authentication Response

---
+++ Bookify/Bookify.UI/bookify/src/app/_interfaces/AuthenticationResponseDto._interface.ts
@@ -0,0 +1,5 @@
+export interface AuthenticationResponseDto{
+    IsAuthSuccessful: boolean,
+    ErrorMessage: string,
+    Token: string,
+}
---
+++ Bookify/Bookify.UI/bookify/src/app/_interfaces/UserForAuthenticationDto._interface.ts
@@ -0,0 +1,4 @@
+export interface UserForAuthenticationDto{
+    Email: string,
+    Password: string,
+}

8732c5a9aa7a1bebf6a3d256649606982fc10c7e
OsamaBytes <osama.ahmed@46bytes.com>
2022-07-24 12:42:25 +0500

    Added Check to the admin layout of the logged in user

--- Bookify/Bookify.UI/bookify/src/app/layouts/admin/admin.component.ts
+++ Bookify/Bookify.UI/bookify/src/app/layouts/admin/admin.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { StorageService } from "src/app/services/Shared/storage.service";
 
 @Component({
   selector: "app-admin",
   templateUrl: "./admin.component.html",
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  constructor(private route: Router, private storage: StorageService) {
+    console.log(storage.Get('token'));
+    if(storage.Get('token') == null){
+      this.route.navigate(['']);
+    }
+  }
 
   ngOnInit(): void {}
 }
